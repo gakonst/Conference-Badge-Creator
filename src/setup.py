4079ced8e4a772153c151a118ce27e181cb0d235
Georgios Konstantopoulos <geokonst95@gmail.com>
2016-10-24 20:42:06 +0300

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,8 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Parse data from excel or dictionary.txt file and print it on your badge. Afterwards, if multiple badges created, combine them in a NxN grid for easier and more economical printing
-    ',
+    'description': 'Parse data from excel or dictionary.txt file and print it on your badge. Afterwards, if multiple badges created, combine them in a NxN grid for easier and more economical printing.',
     'author': 'Georgios Konstantopoulos',
     'url': 'github.com/1337GAK/Conference-Badge-Creator',
     'download_url': 'Where to download it.',
